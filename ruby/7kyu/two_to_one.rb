90adff27e08456f8ea91b771ed28cc60f207e74d
Tommy <t@tommyhoang.com>
2018-12-14 10:40:50 -0800

    two_to_one 7kyu

---
+++ ruby/7kyu/two_to_one.rb
@@ -0,0 +1,13 @@
+#reflection: use chars to split a string into its individual characters
+
+#instructions
+# Take 2 strings s1 and s2 including only letters from ato z. Return a new sorted string, the longest possible, containing distinct letters,
+
+# each taken only once - coming from s1 or s2. #Examples: ``` a = "xyaabbbccccdefww" b = "xxxxyyyyabklmopq" longest(a, b) -> "abcdefklmopqwxy"
+# a = "abcdefghijklmnopqrstuvwxyz" longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" ```
+
+#Initial correct solution
+def longest(a1, a2)
+  both_strings = a1 + a2
+  both_strings.split('').uniq.sort.join('')
+end
