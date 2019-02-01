09400b3cd82a2e18978137f5969963f30979fab3
Tommy <t@tommyhoang.com>
2019-01-31 16:34:51 -0800

    complete index_pivot

--- techScreens/index_pivot.rb
+++ techScreens/index_pivot.rb
@@ -1,26 +1,36 @@
-require 'pry'
+#Reflection: Focus on readability. left_half is only used once so we didn't need to declare it.
 
-def find_pivot(number_list, mid_index = (number_list.length / 2).round)
-
-	left_half = number_list[0...mid_index]
-	left_sum = left_half.sum
-
-	right_half = number_list[mid_index + 1..number_list.length - 1] # we don't to count the potential pivot element
-	right_sum = right_half.sum
-	
-	# binding.pry
-	# return -1 if (left_sum != right_sum) && (mid_index == 1 || mid_index == number_list.length - 2)
-
-	if left_sum == right_sum
-		return mid_index
-	elsif left_sum > right_sum
-		return -1 if mid_index == 1
-		return find_pivot(number_list, mid_index - 1)
-	else
-		return -1 if mid_index == number_list
-		return find_pivot(number_list, mid_index + 1)
+# a cleaner solution
+def find_pivot(numbers)
+	sum = numbers.sum
+	left_sum = 0
+	numbers.each_with_index do |num, index|
+		if left_sum == sum - num - left_sum
+			return index
+		end
+		left_sum += num
 	end
+	return -1
 end
+
+#initial solution
+# def find_pivot(number_list, mid_index = (number_list.length / 2).round)
+# 	left_half = number_list[0...mid_index]
+# 	left_sum = left_half.sum
+
+# 	right_half = number_list[mid_index + 1..number_list.length - 1]
+# 	right_sum = right_half.sum
+	
+# 	return -1 if (left_sum != right_sum) && (mid_index == 1 || mid_index == number_list.length - 2)
+
+# 	if left_sum == right_sum
+# 		return mid_index
+# 	elsif left_sum > right_sum
+# 		return find_pivot(number_list, mid_index - 1)
+# 	else
+# 		return find_pivot(number_list, mid_index + 1)
+# 	end
+# end
 
 # Test Suite
 describe "find_pivot method" do
@@ -28,16 +38,20 @@ describe "find_pivot method" do
 		expect(find_pivot([1, 4, 6, 3, 5])).to eql(-1)
 	end
 
-	xit "should work for arrays with pivot" do
+	it "should work for arrays with pivot" do
 		expect(find_pivot([1, 4, 6, 3, 2])).to eql(2)
 	end	
 
-	xit "should account for pivots at the far right" do
+	it "should account for pivots at the far right" do
 		expect(find_pivot([0, 3, 1, 2, 4])).to eql(3)
 	end
 
 	it "should account for arrays with even elements" do
-		expect(find_pivot([20, 40, 10, 10])).to eql(1)
+		expect(find_pivot([70, 40, 10, 30, 20, 10])).to eql(1)
+	end
+
+	it "should work for an array with 2 elements" do
+		expect(find_pivot([20, 20])).to eql(-1)
 	end
 
 	it "should work for arrays with negative numbers" do
